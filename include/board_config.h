533f42dafa95f832e69b451cdbfad7c37549c685
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-11-04 23:47:35 -0700

    Update

--- include/board_config.h
+++ include/board_config.h
@@ -3,26 +3,44 @@
 
 // Device stuff
 #define HOJA_DEVICE_ID  0xF001
-#define HOJA_FW_VERSION 0x0A05
-
-// RGB Stuff
-//#define HOJA_RGB_PIN 15
-//#define HOJA_RGB_COUNT 20
-//#define HOJA_RGBW_EN 0
+#define HOJA_FW_VERSION 0x0A00
 
 // GPIO definitions
 #define HOJA_SERIAL_PIN 28
 #define HOJA_CLOCK_PIN 8
 #define HOJA_LATCH_PIN 9
 
-// If we do not have native analog triggers
-// set this to zero
-#define HOJA_ANALOG_TRIGGERS 0
-#define HOJA_ANALOG_LIGHT 50
-
 // URL that will display to open a config tool
 #define HOJA_WEBUSB_URL     "handheldlegend.github.io/hoja_config"
 #define HOJA_MANUFACTURER   "Phob"
 #define HOJA_PRODUCT        "Phob 2.1"
+
+#define HOJA_CAPABILITY_ANALOG_STICK_L 1
+#define HOJA_CAPABILITY_ANALOG_STICK_R 1
+#define HOJA_CAPABILITY_ANALOG_TRIGGER_L 0
+#define HOJA_CAPABILITY_ANALOG_TRIGGER_R 0
+
+#define HOJA_CAPABILITY_BLUETOOTH 0
+#define HOJA_CAPABILITY_BATTERY 0
+#define HOJA_CAPABILITY_RGB 0
+#define HOJA_CAPABILITY_GYRO 0
+
+#define HOJA_CAPABILITY_NINTENDO_SERIAL 1
+#define HOJA_CAPABILITY_NINTENDO_JOYBUS 1
+
+#define HOJA_CAPABILITY_RUMBLE 1
+
+#define HOJA_RGB_GROUP_RS       {-1}
+#define HOJA_RGB_GROUP_LS       {-1}
+#define HOJA_RGB_GROUP_DPAD     {-1}
+#define HOJA_RGB_GROUP_MINUS    {-1}
+#define HOJA_RGB_GROUP_CAPTURE  {-1}
+#define HOJA_RGB_GROUP_HOME     {-1}
+#define HOJA_RGB_GROUP_PLUS     {-1}
+#define HOJA_RGB_GROUP_Y        {-1}
+#define HOJA_RGB_GROUP_X        {-1}
+#define HOJA_RGB_GROUP_A        {-1}
+#define HOJA_RGB_GROUP_B        {-1}
+
 
 #endif
--- src/app_rumble.c
+++ src/app_rumble.c
@@ -2,47 +2,90 @@
 #include "interval.h"
 #include "main.h"
 
-bool rumble_on = false;
-bool ramp_up = false;
-bool ramp_down = false;
 const uint32_t _rumble_interval = 8000;
 
-#define RUMBLE_MAX 145
+
+int _rumble_cap = 0;
+
+int _rumble_floor = 0;
+
+int _rumble_min = 0;
+int _rumble_current = 0;
+
+#define RUMBLE_MAX 100
+static uint8_t _rumble_max = RUMBLE_MAX;
+
+static bool _declining = false;
+
+/**
+ * pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, 0);
+ * pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, _rumble_current);
+*/
 
 void app_rumble_task(uint32_t timestamp)
 {
-    static int lvl = 0;
-
     if(interval_run(timestamp, _rumble_interval))
     {
-        if (rumble_on)
+        if (_rumble_current < _rumble_cap)
         {
-            lvl += 25;
-            if (lvl >= RUMBLE_MAX)
-            {
-                lvl = RUMBLE_MAX;
-            }
-            pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, 0);
-            pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, lvl);
+            _rumble_current += 20;
         }
         else
         {
-            lvl -= 20;
-            if (lvl <= 0)
+            _rumble_current -= 10;
+            
+            if (_rumble_current <= _rumble_min)
             {
-                lvl = 0;
-                pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, 0);
-                pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, 255);
+                _rumble_current = _rumble_min; 
             }
-            else
-            {
-                pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, lvl);
-            } 
+
+            _rumble_cap = _rumble_current;
         }
+        
+        pwm_set_gpio_level(PGPIO_RUMBLE_BRAKE, (!_rumble_current) ? 255 : 0);
+        pwm_set_gpio_level(PGPIO_RUMBLE_MAIN, (_rumble_current > 0) ? _rumble_current : 0);
     }
 }
 
-void cb_hoja_rumble_enable(bool enable)
+void cb_hoja_rumble_enable(float intensity)
 {
-    rumble_on = enable;
+    if(intensity > 1.0f) intensity = 1.0f;
+
+    float p = (_rumble_max * intensity);
+    uint16_t tmp = (uint16_t) p;
+
+
+    if(intensity > 0)
+    {
+        tmp += _rumble_floor;
+    }
+
+    _rumble_min = tmp;
+
+    if(tmp>_rumble_cap)
+    {
+        _rumble_cap = tmp;
+    }
+}
+
+void cb_hoja_set_rumble_intensity(uint8_t floor, uint8_t intensity)
+{   
+    floor       = (floor > 75)      ? 75 : floor;
+
+    intensity   = (intensity > 50)  ? 50 : intensity;
+
+    if(!intensity)
+    {
+        _rumble_floor = 0;
+        _rumble_max = 0;
+    }
+    else
+    {
+        _rumble_floor = floor;
+        _rumble_max = intensity;
+    }
+    
+    cb_hoja_rumble_enable(1);
+    sleep_ms(350);
+    cb_hoja_rumble_enable(0);
 }
